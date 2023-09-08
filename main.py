aebd5cf4c8e1db0aa93eea3c652277520231c8dc
yifeidin <yifeidin@cisco.com>
2023-09-08 09:49:07 +0800

    date: 0908

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
@@ -14,15 +15,41 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 @app.get('/')
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get('/items/{item_id}')
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.put('/items/{item_id}')
-def update_item(item_id: int, item: Item):
-    return {"item_id": item_id, "item_name": item.price}
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name}
+
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    print(type(model_name))
+    print(model_name)
+    print(model_name.value)
+    if model_name is ModelName.alexnet:
+        return {
+            "model_name": model_name,
+            "message": "Deep Learning FTW!"
+        }
+    if model_name.value == "resnet":
+        return {
+            "model_name": model_name,
+            "message": "LeCNN all the images"
+        }
+
+    return {"model_name": model_name, "message": "Have some residuals"}
